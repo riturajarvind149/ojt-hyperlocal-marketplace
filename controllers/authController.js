d1129230aafd4e2ed67c058772a69d1a68bba7ef
Ritu RAj Arvind <ritu.p25@medhaviskillsuniversity.edu.in>
2026-03-25 11:59:29 +0530

    Completed authentication module with JWT, middleware, and protected routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const Student = require("../models/Student");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Student Registration
 exports.registerStudent = async (req, res) => {
@@ -75,3 +76,52 @@ exports.registerBusiness = async (req,res)=>{
 
 }
 
+exports.login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    // check student
+    let user = await Student.findOne({ email });
+
+    let role = "student";
+
+    if (!user) {
+      user = await Business.findOne({ email });
+      role = "business";
+    }
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found"
+      });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid password"
+      });
+    }
+
+    // generate token
+    const token = jwt.sign(
+      { id: user._id, role: role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      role
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerStudent, registerBusiness } = require("../controllers/authController");
+const { registerStudent, registerBusiness, login } = require("../controllers/authController");
+const { verifyToken } = require("../middleware/authMiddleware");
 
+// routes
 router.post("/register-student", registerStudent);
 router.post("/register-business", registerBusiness);
+router.post("/login", login);
+
+router.get("/protected", verifyToken, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user
+  });
+});
 
 module.exports = router;
